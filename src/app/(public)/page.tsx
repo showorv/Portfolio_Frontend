68e51eaa674af0b202561443f49ce71664aee098
Showrov <yshowrov7@gmail.com>
2025-10-06 23:42:34 +0600

    folder structure initialize

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2 className="text-4xl">Welcome to portfolio</h2>
+    </div>
+  );
+}
---
+++ src/app/(public)/projects/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SingleProjectPage() {
+  return (
+    <div>SingleProjectPage id 1</div>
+  )
+}
