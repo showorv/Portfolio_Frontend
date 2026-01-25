da5221bfa06cc57742e1e8bcf47d3cbd905277ae
Showrov <yshowrov7@gmail.com>
2026-01-25 20:58:49 +0600

    deploy

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,6 +1,6 @@
 import { FieldValues } from "react-hook-form";
 import { setCookie } from "./tokenHandle";
-
+import { parse } from "cookie";
 
 export const login= async (data: FieldValues) => {
   let accessTokenObject: null | any = null;
@@ -21,6 +21,28 @@ export const login= async (data: FieldValues) => {
     console.log(res?.text() || "Login failed");
   }
 
+  const setCookieHeaders = res.headers.getSetCookie();
+
+        if (setCookieHeaders && setCookieHeaders.length > 0) {
+            setCookieHeaders.forEach((cookie: string) => {
+                const parsedCookie = parse(cookie);
+
+                if (parsedCookie['accessToken']) {
+                    accessTokenObject = parsedCookie;
+                   
+                    
+                }
+              
+            })
+        } else {
+            throw new Error("No Set-Cookie header found");
+        }
+
+        if (!accessTokenObject) {
+            throw new Error("Tokens not found in cookies");
+        }
+
+      
   await setCookie("accessToken", accessTokenObject.accessToken, {
     secure: true,
     httpOnly: true,
