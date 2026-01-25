16334d952e16748793a22b573576a35efc756481
Showrov <yshowrov7@gmail.com>
2026-01-25 21:02:34 +0600

    deploy

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -3,7 +3,7 @@ import { setCookie } from "./tokenHandle";
 import { parse } from "cookie";
 
 export const login= async (data: FieldValues) => {
-  let accessTokenObject: null | any = null;
+  // let accessTokenObject: null | any = null;
 
   const res = await fetch (`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth`,{
         method: "POST",
@@ -21,35 +21,35 @@ export const login= async (data: FieldValues) => {
     console.log(res?.text() || "Login failed");
   }
 
-  const setCookieHeaders = res.headers.getSetCookie();
+//   const setCookieHeaders = res.headers.getSetCookie();
 
-        if (setCookieHeaders && setCookieHeaders.length > 0) {
-            setCookieHeaders.forEach((cookie: string) => {
-                const parsedCookie = parse(cookie);
+//         if (setCookieHeaders && setCookieHeaders.length > 0) {
+//             setCookieHeaders.forEach((cookie: string) => {
+//                 const parsedCookie = parse(cookie);
 
-                if (parsedCookie['accessToken']) {
-                    accessTokenObject = parsedCookie;
+//                 if (parsedCookie['accessToken']) {
+//                     accessTokenObject = parsedCookie;
                    
                     
-                }
+//                 }
               
-            })
-        } else {
-            throw new Error("No Set-Cookie header found");
-        }
+//             })
+//         } else {
+//             throw new Error("No Set-Cookie header found");
+//         }
 
-        if (!accessTokenObject) {
-            throw new Error("Tokens not found in cookies");
-        }
+//         if (!accessTokenObject) {
+//             throw new Error("Tokens not found in cookies");
+//         }
 
       
-  await setCookie("accessToken", accessTokenObject.accessToken, {
-    secure: true,
-    httpOnly: true,
-    maxAge: parseInt(accessTokenObject['Max-Age']) || 1000 * 60 * 60,
-    path: accessTokenObject.Path || "/",
-    sameSite: accessTokenObject['SameSite'] || "none",
-});
+//   await setCookie("accessToken", accessTokenObject.accessToken, {
+//     secure: true,
+//     httpOnly: true,
+//     maxAge: parseInt(accessTokenObject['Max-Age']) || 1000 * 60 * 60,
+//     path: accessTokenObject.Path || "/",
+//     sameSite: accessTokenObject['SameSite'] || "none",
+// });
   return result;
     
     
